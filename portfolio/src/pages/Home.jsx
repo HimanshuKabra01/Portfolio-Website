86ea7266c4e2a2cfd6fc54714096c93084fe74f0
Himanshu Kabra <kabrahimanshu123@gmail.com>
2025-07-20 15:26:26 +0530

    fix working of tailwindcss and apply background

--- portfolio/src/pages/Home.jsx
+++ portfolio/src/pages/Home.jsx
@@ -3,10 +3,15 @@ import LiveBackground from "../components/LiveBackground";
 export default function Home() {
 
   return(
-    <div>
-      <div className="font-primary">
-        <LiveBackground />
+    <div className="w-full min-h-screen flex flex-col">
+      <div className="w-full flex flex-col flex-1 px-4"> 
+        <div className="flex flex-col items-center justify-center flex-1 relative">
+        </div>
+        <div className="absolute inset-0 -z-10 overflow-hidden">
+          <LiveBackground />
+        </div>
       </div>
+      
     </div>
   );
 };
